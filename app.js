d3537ba58184f4b15db48e46cdc7ed47634aec81
Fulya <fulya.ertay@gmail.com>
2022-12-16 20:13:26 +0300

    feat:add filter

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-
-//Add task on the form
+//Defining variables
 const form=document.querySelector(".add-form");
 const todoList=document.querySelector(".todos");
+const searchInput=document.querySelector('.search input');
 
+
+//Search any task
+searchInput.addEventListener('keyup',e=>{
+    const searchValue=searchInput.value.trim().toLowerCase();
+    createFilter(searchValue);
+
+})
+
+const createFilter=(searchValue)=>{
+    Array.from(todoList.children).filter((todos)=>{
+        return todos.textContent.includes(searchValue);
+    })
+
+}
+//Add task on the form
 function createTemplate(task){
     let html=`
     <li class="list-group-item d-flex justify-content-between align-items-center">
@@ -18,7 +33,7 @@ function createTemplate(task){
 form.addEventListener('submit',e=>{
     e.preventDefault();
 
-    const todo=form.add.value.trim();
+    const todo=form.add.value.trim().toLowerCase;
     if (todo.length){
         createTemplate(todo);
         form.reset();
